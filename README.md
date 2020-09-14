30858c62bee568d3ac4051a4b9051b6a3f515b24
statarm <artur.mrozowski@tryg.dk>
2020-09-14 06:55:51 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# xmlparser
+Java / Kotlin app able to process huge xml files.
+ Currently it takes gzipped xml file of some logs from wikimedia. Currently it process dansk motorregister(DMR) 80GB XML-file under 19 minutes. 
+
+# Build
+Run gradle `jar` build task
+
+# Run
+DMR file is provided by Danish Tax Authorities. You can use the sample file 
+and then point it in the command run:
+
+`java -jar build/libs/xmlparser-1.0.0.jar file-path=test10.xml.gz file-name=test10.xml job-chunk-size=10`
---
+++ src/main/kotlin/xmlparser/script.kt
@@ -0,0 +1,143 @@
+package xmlparser
+
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.FileInputStream
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+import java.util.jar.JarInputStream
+import java.util.zip.GZIPInputStream
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipFile
+import javax.xml.stream.XMLInputFactory
+import javax.xml.stream.XMLStreamConstants
+import javax.xml.stream.events.StartElement
+import kotlin.concurrent.thread
+
+typealias XmlSingleData = String
+
+
+fun main(args: Array<String>) {
+    val logger = LoggerFactory.getLogger("XML Parser")
+    val debugger = Profiler(logger).apply { start() }
+    val arguments = CliArguments(args, mapOf("job-chunk-size" to "800", "concurrent-jobs" to "3"))
+    val concurrentJobs = arguments.get("concurrent-jobs")
+    val jobChunkSize = arguments.get("job-chunk-size")
+    val filePath = arguments.getString("file-path")
+    val fileName = arguments.getString("file-name")
+
+    logger.info("Launching XML Parser with parameters:")
+    logger.info("job-chunk-size set to $jobChunkSize")
+    logger.info("concurrent-jobs set to $concurrentJobs")
+    logger.info("file-path set to $filePath")
+    logger.info("You can set those by passing parameters like 'java -jar xmlparser.jar job-chunk-size=500 concurrent-jobs=6'")
+
+    val dataList = LinkedBlockingQueue<XmlSingleData>(jobChunkSize)
+    val todoList = LinkedBlockingQueue<DataRunnable>(concurrentJobs * 2 + 1)
+
+    /** Thread that reads xml file and fills data list */
+
+      val readerThread = thread(start = true) {
+
+
+        val fileStream = FileInputStream(File(filePath))
+       val gzipStream = GZIPInputStream(fileStream)
+        val xmlReader = XMLInputFactory.newInstance().createXMLStreamReader(gzipStream)
+        val eventReader = XMLInputFactory.newInstance().createXMLEventReader(gzipStream)
+
+       //zip Stream only
+
+      /**  val zipfile = ZipFile(File(filePath))
+        val zipEntry = zipfile.getEntry(fileName)
+        val zipStream = zipfile.getInputStream(zipEntry)**/
+
+     /**   val fileStream = Thread.currentThread().contextClassLoader.getResourceAsStream(filePath)
+        val zipStream = ZipInputStream(fileStream).apply {
+           nextEntry
+        }**/
+       // val eventReader = XMLInputFactory.newInstance().createXMLEventReader(zipStream)
+
+        var isIdent = false
+
+      /**  while (xmlReader.hasNext()) {
+            xmlReader.next()
+            if (xmlReader.eventType == XMLStreamConstants.START_ELEMENT && xmlReader.localName == "username") {
+                dataList.put(xmlReader.elementText)
+
+                //dataList.put(xmlReader.elementText.toString())
+
+            }
+        }**/
+
+         while (eventReader.hasNext()) {
+             val event = eventReader.nextEvent()
+
+             //val startElement = event.asStartElement()
+        if (
+                //event.eventType == XMLStreamConstants.START_ELEMENT
+                event.isStartElement
+                && event.asStartElement().name.localPart == "KoeretoejIdent") {
+        logger.info("I was here")
+
+        // logger.info("$event.asCharacters()")
+           // startElement.getAttributeByName("KoeretoejIdent")
+        //dataList.put(xmlReader.elementText.toString())
+            isIdent = true
+        }
+             if(event.isCharacters && isIdent )
+             {
+                 //logger.info("Ident is true")
+                 isIdent = false
+                 event.asCharacters().data
+                 File("fileout.txt").appendText(event.asCharacters().data+System.lineSeparator())
+
+                 // println( event.asCharacters().data)
+                 dataList.put(event.asCharacters().data)
+             }
+        }
+        logger.info("I am done ${dataList.size}")
+    }
+
+    /** Thread that creates tasks to do (runnables) */
+    thread(start = true) {
+        while (readerThread.isAlive) {
+            if (dataList.remainingCapacity() == 0) {
+                logger.debug("Take ${dataList.size} elements. I have now ${todoList.size} tasks to do")
+                val tmpDataList = mutableListOf<XmlSingleData>()
+                dataList.drainTo(tmpDataList, dataList.size)
+                todoList.put(DataRunnable(tmpDataList))
+            }
+        }
+        if (dataList.size > 0) {
+            logger.debug("Take ${dataList.size} elements. I have now ${todoList.size} tasks to do")
+            val tmpDataList = mutableListOf<XmlSingleData>()
+            dataList.drainTo(tmpDataList, dataList.size)
+            todoList.put(DataRunnable(tmpDataList))
+        }
+    }
+
+    /** Run pool of data executors */
+    @Suppress("UNCHECKED_CAST")
+    val executor = ThreadPoolExecutor(
+        concurrentJobs,
+        concurrentJobs,
+        0L,
+        TimeUnit.MILLISECONDS,
+        todoList as BlockingQueue<Runnable>
+    )
+    executor.prestartAllCoreThreads()
+
+    /** Kill the thread pool when finished and show debug info */
+    thread(start = true) {
+        Thread.sleep(1000)
+        while (executor.activeCount > 0 || readerThread.isAlive) {
+            continue
+        }
+        executor.shutdownNow()
+        debugger.stop()
+        debugger.print()
+    }
+}
